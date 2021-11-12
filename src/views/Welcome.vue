635fd6a61500ef2bf979ea084d909e8930ccdea4
DavidBai <baiminghan@gmail.com>
2021-11-12 22:48:34 +1300

    initial setup

---
+++ src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+}
+</style>
