013bb54eb3d6c55809e9eee0ff813a60cd060db1
DavidBai <baiminghan@gmail.com>
2021-11-14 11:36:22 +1300

    setup the log out function and test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,28 @@
       <p>Hey there... display name here</p>
       <p class="email">Currently logged in as ... email</p>
     </div>
-    <button>Log out</button>
+    <button @click="handleClick">Log out</button>
   </nav>
 </template>
 
 <script>
-export default {};
+import useLogout from "../composables/useLogout";
+
+export default {
+  setup() {
+    const { logout, error } = useLogout();
+
+    const handleClick = async () => {
+      await logout();
+
+      if (!error.value) {
+        console.log("user log out");
+      }
+    };
+
+    return { handleClick };
+  },
+};
 </script>
 
 <style></style>
