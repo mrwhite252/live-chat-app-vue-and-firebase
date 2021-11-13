5a57fca3a85c7dc5db98ced24c552505704ea6ae
DavidBai <baiminghan@gmail.com>
2021-11-13 20:33:30 +1300

    add firebase auth package into config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firbase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBP6yTnTyvf3fcjK1eFVts0XGvXJ1yssAY",
@@ -15,8 +16,10 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+const projectAuth = firebase.auth();
+
 const projectFirestore = firebase.firestore();
 
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, timestamp };
+export { projectAuth, projectFirestore, timestamp };
