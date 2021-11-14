5517b22961e15c6faa6715c07649e57596a6c104
DavidBai <baiminghan@gmail.com>
2021-11-14 21:07:57 +1300

    final clean up and give the chatroom a route guard when the user already logged in

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,17 @@ import { projectAuth } from "../firebase/config";
 
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser;
-  console.log("current user in auth guard: ", user);
   if (!user) {
     next({ name: "Welcome" });
+  } else {
+    next();
+  }
+};
+
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ name: "chatroom" });
   } else {
     next();
   }
@@ -20,6 +28,7 @@ const routes = [
     path: "/",
     name: "Welcome",
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: "/chatroom",
